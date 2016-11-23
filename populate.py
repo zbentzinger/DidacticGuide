30b35e8cd7aca6e8c3e0198e7cbee774549e3f65
Zachary Bentzinger <zbentzinger@kickbackpoints.com>
2016-11-22 20:55:25 -0700

    Refactored initial population module. Should be more efficient now.

--- populate.py
+++ populate.py
@@ -1,12 +1,13 @@
 import requests
 import sqlite3
 import sys
+#import timeit
 
 
 def createDB():
-	"""Creates a sqlite database to store solar wind data in"""
+	"""Creates a sqlite database"""
 	open('data.db', 'a').close()
-	query = """
+	create_statement = """
 		CREATE TABLE 'plasma' (
 		`timestamp` TEXT UNIQUE,
 		`density` REAL,
@@ -14,42 +15,26 @@ def createDB():
 		`temperature`INTEGER
 		);
 	"""
-	executeQuery(query)
-
-def executeQuery(query):
-	"""Executes SQL queries and returns an output array for SELECT statements"""
-	output = []
+	connection = sqlite3.connect('data.db')
 	try:
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-		cursor.execute(query)
-		output = cursor.fetchall()
-		connection.commit()
-		connection.close()
-	except sqlite3.IntegrityError:
-		# Just a duplicate timestamp, no need to crash the program
+		with connection:
+			connection.execute(create_statement)
+	except sqlite3.OperationalError:
+		# Database has already been setup
 		pass
-	finally:
-		return output
 
 def insertData(json):
 	"""Insert data into the sqlite database. Must pass a valid JSON object"""
-	for row in json:
-		date = '"{}"'.format(row[0])
-		density = row[1]
-		speed = row[2]
-		temp = row[3]
-		insert_statement = """
-			INSERT INTO `plasma` (`timestamp`, `density`, `speed`, `temperature`) 
-			VALUES ({}, {}, {}, {});
-		""".format(date, density, speed, temp)
-		executeQuery(insert_statement)
-
-def getNewestEntry():
-	"""return the newest entry in the database based on timestamp"""
-	query = 'select timestamp from plasma order by timestamp desc limit 1;'
-	return executeQuery(query)[0][0]
-
+	insert_statement = """
+		INSERT OR IGNORE INTO `plasma` (`timestamp`, `density`, `speed`, `temperature`) 
+		VALUES (?,?,?,?);
+	"""
+	connection = sqlite3.connect('data.db')
+	try:
+		with connection:
+			connection.executemany(insert_statement, json)
+	except:
+		raise
 
 def retrieveData(url):
 	"""GET the JSON from noaa.gov and format it"""
@@ -61,15 +46,25 @@ def retrieveData(url):
 def main():
 	"""Run the program"""
 	latest = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-5-minute.json'
-	archive = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json'
+	hourly = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-2-hour.json'
+	daily = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-1-day.json'
+	weekly = 'http://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json'
+	
 	try:
 		if sys.argv[1] == 'latest':
 			insertData(retrieveData(latest))
-		elif sys.argv[1] == 'backfill':
-			insertData((retrieveData(archive)))
+		elif sys.argv[1] == 'hour':
+			insertData(retrieveData(hourly))
+		elif sys.argv[1] == 'day':
+			insertData(retrieveData(daily))
+		elif sys.argv[1] == 'week':
+			insertData(retrieveData(weekly))
+		else:
+			sys.exit("Error: InvalidTimePeriod: ('latest','hour','day','week')")
 	except IndexError:
+		# Create the database, and backfill for the last week. Cron will handle the minutae
 		createDB()
-		insertData(retrieveData(archive))
+		insertData(retrieveData(weekly))
 
 if __name__ == '__main__':
 	main()
